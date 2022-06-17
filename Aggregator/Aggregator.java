f1febe78f4996150363b41477c2d4ee1970962d5
Bogdan <51080321+koozzma@users.noreply.github.com>
2022-06-17 14:25:04 +0300

    Add files via upload

---
+++ Aggregator/Aggregator.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task28.task2810;
+
+import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.HabrCareerStrategy;
+import com.javarush.task.task28.task2810.model.Model;
+import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
+import com.javarush.task.task28.task2810.view.View;
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+
+import java.util.List;
+
+public class Aggregator {
+    public static void main(String[] args) {
+
+        HtmlView view = new HtmlView();
+        Model model = new Model(view, new Provider(new HHStrategy()), new Provider(new HabrCareerStrategy()));
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        view.userCitySelectEmulationMethod();
+
+
+    }
+}
